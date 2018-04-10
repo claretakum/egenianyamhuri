b911abb949fc9c1e9ce7b904559755141175ad09
egenia nyamhuri <egenianyamhuri@gmail.com>
2018-04-11 00:06:44 +0200

    added composer.json file

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php echo file_get_contents("html/header.html"); ?>
+<title>Welcome to Anashe</title>
+<!--?php echo file_get_contents("html/header2.html"); ?-->
+Anashe is a toddler who is very naughty and interesting.
+<?php echo file_get_contents("html/footer.html"); ?>
---
+++ page3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Anashe</title>
+        <meta name="description" contents="Just a test website for learning html, css and php">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <header>
+            <nav id="main-navigation">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="page2.php">Page 2</a></li>
+                    <li><a href="page3.php">Page 3</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id="main-contents">
+            The third page concludes on about Anashe my lovely daughter.
+        </div>
+        <footer>
+            Contact us at info@anashe.com
+        </footer>
+    </body>
+</html>
