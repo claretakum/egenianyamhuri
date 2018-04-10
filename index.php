7d6a3a4eecd241e8e445fa85f25de9d17ec8ba2a
sico14 <sico14@users.noreply.github.com>
2018-04-11 00:43:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php echo file_get_contents("html/header.html"); ?>
-<title>Welcome to Anashe</title>
+<title>Welcome to Anashe,Egenia Nyamhuri's lovely daughter</title>
 <!--?php echo file_get_contents("html/header2.html"); ?-->
 Anashe is a toddler who is very naughty and interesting.
 <?php echo file_get_contents("html/footer.html"); ?>
